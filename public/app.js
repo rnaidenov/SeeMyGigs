ba333691a31b3af6b9880ff1c9e1b393fe8cef6d
Radoslav Naydenov <radoslav.naydenov@kcl.ac.uk>
2016-10-28 18:44:41 +0100

    Add service for getting data from external API

--- public/app.js
+++ public/app.js
@@ -1,98 +1,173 @@
-var app = angular.module ("myApp",[]);
+var app = angular.module("myApp", []);
 
-app.directive ("welcome",function () {
-    return {
-        restrict : "E",
-        templateUrl : "views/welcome.html",
-        controller : function ($scope) {
+app.service("EventsService", ["$http", function ($http) {
 
-            $scope.searchOptions = [
-                {
-                    id : "city",
-                    imgUrl : "resources/cityIcon.png",
-                    imgUnclicked : "resources/cityIcon.png",
-                    imgClicked : "resources/cityIconClicked.png",
-                    tooltip : "Click to search by city",
-                    isClicked : false
-                },
-                {
-                    id : "artist",
-                    imgUrl : "resources/artistIcon.png",
-                    imgUnclicked : "resources/artistIcon.png",
-                    imgClicked : "resources/artistIconClicked.png",
-                    tooltip : "Click to search by artist",
-                    isClicked : false
-                },
-                {
-                    id : "venue",
-                    imgUrl : "resources/venueIcon.png",
-                    tooltip : "Click to search by venue",
-                    isClicked : false
-                }
-            ]
+    this.searchByCity = function (city) {
+        return $http.get("http://api.gigatools.com/city.json?cities[]=" + city + "&api_key=d924857077bc386767")
+            .then(function (response) {
+                return response.data[1];
+            })
+    }
 
 
-            $scope.searchBy = "Select option to search by";
-            $scope.selected = null;
-            $scope.dropdown_image =  "resources/dropdown_menu.png";
+    this.searchByArtist = function (artist) {
+        return $http.get("http://api.gigatools.com//gigs.json?users[]=" + artist + "&api_key=d924857077bc386767")
+            .then(function (response) {
+                var allEvents =  response.data[1];
+                var filteredEvents = [];
+                allEvents.forEach(function (singleEvent) {
+                    if (singleEvent.event_owner === artist) {
+                        filteredEvents.push(singleEvent);
+                    }
+                })
+                return filteredEvents;
+            })
+    }
 
+    this.searchByVenue = function (venue) {
+        return $http.get("http://api.gigatools.com/venue.json?venues[]=" + venue + "&api_key=d924857077bc386767")
+            .then(function (response) {
+                return response.data[1];
+            })
+    }
 
-            //If there is any user input indicate that
-            // program is ready to perform a search
-            $scope.newInput = function () {
-                if ($scope.searchInput !== "") {
-                    $scope.dropdown_image =  "resources/searchIcon.png";
-                    return true;
-                }
-                else {
-                    $scope.dropdown_image = "resources/dropdown_menu.png";
-                    return false;
-                }
+}])
 
-            }
+app.controller("myCtrl",function ($scope,EventsService) {
 
-            //Check if there is a search filter selected
-            $scope.optionSelected = function () {
-                if ($scope.selected !== null) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-
-            //Toggle the image when selected
-            $scope.getImage = function (option) {
-                if (option.isClicked) {
-                    option.imgUrl = "resources/" + option.id + "IconClicked.png";
-                }
-                else {
-                    option.imgUrl = "resources/" + option.id + "Icon.png";
-                }
-                return option.imgUrl;
-            }
-
-
-            $scope.getResults = function () {
-                console.log("Code for getting results is here.");
-            }
-
-
-            //Updates the option that is selected
-            $scope.selectOption = function (option) {
-                $scope.searchBy = "Search by " + option.id;
-                if ($scope.selected === null) {
-                    $scope.selected = option;
-                    option.isClicked = true;
-                }
-                else  {
-                    var previousSelected;
-                    previousSelected = $scope.selected;
-                    $scope.selected = option;
-                    previousSelected.isClicked = false;
-                    option.isClicked = true;
-                }
-            }
+    $scope.searchOptions = [
+        {
+            id: "city",
+            imgUrl: "resources/cityIcon.png",
+            imgUnclicked: "resources/cityIcon.png",
+            imgClicked: "resources/cityIconClicked.png",
+            tooltip: "Click to search by city",
+            isClicked: false
+        },
+        {
+            id: "artist",
+            imgUrl: "resources/artistIcon.png",
+            imgUnclicked: "resources/artistIcon.png",
+            imgClicked: "resources/artistIconClicked.png",
+            tooltip: "Click to search by artist",
+            isClicked: false
+        },
+        {
+            id: "venue",
+            imgUrl: "resources/venueIcon.png",
+            tooltip: "Click to search by venue",
+            isClicked: false
         }
-    };
+    ]
+
+
+    $scope.searchBy = "Select option to search by";
+    $scope.selected = null;
+    $scope.dropdown_image = "resources/dropdown_menu.png";
+
+
+    //If there is any user input indicate that
+    // program is ready to perform a search
+    $scope.newInput = function () {
+        if ($scope.searchInput !== "") {
+            $scope.dropdown_image = "resources/searchIcon.png";
+            return true;
+        }
+        else {
+            $scope.dropdown_image = "resources/dropdown_menu.png";
+            return false;
+        }
+
+    }
+
+    //Check if there is a search filter selected
+    $scope.optionSelected = function () {
+        if ($scope.selected !== null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    //Toggle the image when selected
+    $scope.getImage = function (option) {
+        if (option.isClicked) {
+            option.imgUrl = "resources/" + option.id + "IconClicked.png";
+        }
+        else {
+            option.imgUrl = "resources/" + option.id + "Icon.png";
+        }
+        return option.imgUrl;
+    }
+
+    //Search function that uses the EventsService for getting data from API
+    $scope.search = function (input) {
+
+
+        switch ($scope.selected.id) {
+            case "city" :
+                console.log("Have to search by city.");
+                EventsService.searchByCity(input)
+                    .then(function (events) {
+                        events.forEach(function (singleEvent) {
+                            console.log(singleEvent);
+                        })
+                    })
+                break;
+
+            case "artist" :
+                console.log("Have to search by artist.");
+                EventsService.searchByArtist(input)
+                    .then(function (events) {
+                        events.forEach(function (singleEvent) {
+                            console.log(singleEvent);
+                        })
+                    })
+                break;
+
+            case "venue" :
+
+                console.log("Have to search by venue.");
+                EventsService.searchByVenue(input)
+                    .then(function (events) {
+                        events.forEach(function (singleEvent) {
+                            console.log(singleEvent);
+                            if (singleEvent.event_owner === "Ben Klock") {
+                                console.log("JACKPOT!");
+                            }
+                        })
+                    })
+                break;
+
+        }
+
+    }
+
+
+    //Updates the option that is selected
+    $scope.selectOption = function (option) {
+        $scope.searchBy = "Search by " + option.id;
+        if ($scope.selected === null) {
+            $scope.selected = option;
+            option.isClicked = true;
+        }
+        else {
+            var previousSelected;
+            previousSelected = $scope.selected;
+            $scope.selected = option;
+            previousSelected.isClicked = false;
+            option.isClicked = true;
+        }
+    }
+})
+
+
+app.directive("welcome", function () {
+    return {
+        restrict: "E",
+        templateUrl: "views/welcome.html",
+        controller: "myCtrl"
+    }
 })
--- server/server.js
+++ server/server.js
@@ -1,3 +1,24 @@
+var express = require("express");
+var cors = require("cors");
+
+var app = express();
+
+
 var mongoose = require ("mongoose");
 mongoose.connect("mongodb://localhost/myEvents");
 
+var connect = require("connect");
+var serveStatic = require("serve-static");
+
+// var app = connect();
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(serveStatic("../public"));
+
+app.listen(3000);
